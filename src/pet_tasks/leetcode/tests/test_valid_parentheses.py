342b76add18f89f27c9d566daa2b5ccf03c6b67f
Stefan <sfilatov@MacBook-Pro--sfilatov.local>
2025-12-08 00:02:05 +0300

    Valid parentheses

--- src/pet_tasks/leetcode/tests/test_valid_parentheses.py
+++ src/pet_tasks/leetcode/tests/test_valid_parentheses.py
@@ -6,11 +6,14 @@ from pet_tasks.leetcode.valid_parentheses import valid_parentheses
 @pytest.mark.parametrize(
     ('text', 'exp_res'),
     [
+        ('(', False),
         ('()', True),
         ('()[]{}', True),
         ('(]', False),
         ('([])', True),
         ('([)]', False),
+        ('((', False),
+        ('){', False),
     ],
 )
 def test_valid_parentheses(text: str, exp_res: bool) -> None:
--- src/pet_tasks/leetcode/valid_parentheses.py
+++ src/pet_tasks/leetcode/valid_parentheses.py
@@ -1,2 +1,21 @@
 def valid_parentheses(text: str) -> bool:
     """Проверяет корректность скобок."""
+    if len(text) % 2 != 0:
+        return False
+
+    p_map = {
+        ')': '(',
+        ']': '[',
+        '}': '{',
+    }
+
+    open_p: list[str] = []
+    for p_ in text:
+        if op_p := p_map.get(p_):
+            curr_open = open_p.pop(-1) if open_p else None
+            if op_p != curr_open:
+                return False
+        else:
+            open_p.append(p_)
+
+    return len(open_p) == 0
